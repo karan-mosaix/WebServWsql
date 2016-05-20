63b851e1cba223a21ce4274317d950b104cd360f
Karan <karan7dm@gmail.com>
2016-05-20 14:06:45 -0700

    Inited git

---
+++ httpWform.js
@@ -0,0 +1,66 @@
+var http = require('http');
+var fs = require('fs');
+var formidable = require("formidable");
+var util = require('util');
+//var mysql = require("mysql");
+var sql = require('./sql');
+
+var server = http.createServer(function (req, res) {
+    if (req.method.toLowerCase() == 'get') {
+/*	// lets see how request looks like:
+	console.log('request:\n');
+	for(var key in req){
+		console.log("\n" + key);
+	}
+*/
+        displayForm(res);
+    } else if (req.method.toLowerCase() == 'post') {
+        processAllFieldsOfTheForm(req, res);
+    }
+
+});
+
+function displayForm(res) {
+    fs.readFile('form.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+                'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
+
+// will be called upon form submission:
+function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    // get form input and modify an html page for it:
+    form.parse(req, function (err, fields, files) {
+      var req_id = parseInt(fields.ID);
+
+       // access mysql server and retrieve the name:
+      console.log("response from name_from_sql: " + resp)
+      sql.name_from_sql(req_id, function(resp){
+          fs.readFile('form.html','utf8', function (err, data) {
+            res.writeHead(200, {
+                            'Content-Type': 'text/html'
+                        });
+          // parse lines to modify the html and put the response there:
+            html_lines = data.split('\n');
+            for (var i in html_lines){
+              // edit the last line before body ends:
+              if (html_lines[i].trim()=='</body>'){
+                res.write("This is your response: " + resp);
+              }
+              res.write(html_lines[i]);
+
+            }
+            res.end();
+          }); // end of readfile ( done with creating html)
+      }); // end name_from_sql
+    }); // end of form parse
+}
+
+server.listen(1185);
+console.log("server listening on 1185");
---
+++ sql.js
@@ -0,0 +1,46 @@
+var mysql = require("mysql");
+
+//export functions:
+
+module.exports = {
+	//retrieve a name corresponding to an id in mysql:
+	 name_from_sql: function(id, callback){
+				// create a connection to databse:
+				var ret_name;
+				var con = mysql.createConnection({
+					host: "localhost",
+					// change these to match yours:
+					user: "root",
+					password: "qwe213",
+					database: "some_name"
+				});
+
+				// connect to sql:
+				con.connect(function(err){
+					if(err){
+					  console.log('Error connectiing to db');
+					  return;
+					}
+					console.log('Connection established');
+				});
+
+				// query data:
+				con.query('SELECT * FROM potluck WHERE id='+id,function(err,rows){
+					if(err){
+					   throw err;
+					   return;
+					}
+					console.log('Data received from db:\n');
+					/*for (var i=0;i<rows.length;i++){
+					  console.log(rows[i].food);
+					}*/
+					console.log(rows[0].name)
+					ret_name = rows[0].name;
+					callback(ret_name); // call the call back function with return user name
+				});
+
+				con.end(function(err) {
+					// gracefull disconnection
+				});
+	 }
+}; //module_exports
