5b4aa835c6f6d25d89a0e5e24f39e74d756cc43c
Karan <karan7dm@gmail.com>
2016-05-20 14:42:35 -0700

    added commandline support

--- httpWform.js
+++ httpWform.js
@@ -16,8 +16,9 @@ var server = http.createServer(function (req, res) {
         displayForm(res);
     } else if (req.method.toLowerCase() == 'post') {
         processAllFieldsOfTheForm(req, res);
+    } else if (req.method.toLowerCase() == 'put'){ // request from commandline
+        processCurlMessage(req, res);
     }
-
 });
 
 function displayForm(res) {
@@ -44,10 +45,11 @@ function processAllFieldsOfTheForm(req, res) {
           fs.readFile('form.html','utf8', function (err, data) {
             console.log("response from name_from_sql: " + resp);
             res.writeHead(200, {
-                            'Content-Type': 'text/html'
+                            'Content-Type': 'text/html',
+                                'Content-Length': resp.length
                         });
           // parse lines to modify the html and put the response there:
-            html_lines = data.split('\n');
+            var html_lines = data.split('\n');
             for (var i in html_lines){
               // edit the last line before body ends:
               if (html_lines[i].trim()=='</body>'){
@@ -60,7 +62,22 @@ function processAllFieldsOfTheForm(req, res) {
           }); // end of readfile ( done with creating html)
       }); // end name_from_sql
     }); // end of form parse
-}
+} //end processAllFieldsOfTheForm
+
+
+// will be called upon command line curl call:
+function processCurlMessage(req, res){
+  var rand_id = Math.floor(Math.random()*4+1)
+  sql.name_from_sql(rand_id, function(ret_name){
+    res.writeHead(200, {
+                    'Content-Type': 'text/plain',
+                        'Content-Length': ret_name.length+1
+                });
+    res.end(ret_name + '\n');
+  }); // end name_from_sql
+
+} // end processCurlMessage
+
 
 server.listen(1185);
 console.log("server listening on 1185");
